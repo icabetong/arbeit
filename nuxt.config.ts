45b4d157d3dce65e22eaa0f75faabffea483e339
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2023-06-25 16:26:27 +0800

    Setup i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: ["@nuxtjs/i18n"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  css: ["@/assets/css/main.css"],
-});
+	devtools: { enabled: false },
+	modules: ['@nuxtjs/i18n'],
+	i18n: {
+		lazy: false,
+		langDir: 'locales',
+		defaultLocale: 'en',
+		locales: [
+			{ code: 'en', file: 'en-US.json' },
+			{ code: 'fr', file: 'fr-FR.json' }
+		]
+	},
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {}
+		}
+	},
+	css: ['@/assets/css/main.css']
+})
